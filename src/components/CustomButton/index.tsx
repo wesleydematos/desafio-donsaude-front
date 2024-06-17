2b135ca8c6c4172170ed6e9d86fee729790fb59f
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-06-17 10:05:53 -0300

    feat: adjust props

--- src/components/CustomButton/index.tsx
+++ src/components/CustomButton/index.tsx
@@ -2,17 +2,10 @@ import { FC, ReactNode } from "react";
 import { Button as ChakraButton, ButtonProps } from "@chakra-ui/react";
 
 interface CustomButtonProps extends ButtonProps {
-  action: () => void;
   children: ReactNode;
-  isLoading: boolean;
 }
 
-const CustomButton: FC<CustomButtonProps> = ({
-  action,
-  children,
-  isLoading,
-  ...props
-}) => {
+const CustomButton: FC<CustomButtonProps> = ({ children, ...props }) => {
   return (
     <ChakraButton
       bg="primary.base"
@@ -21,8 +14,6 @@ const CustomButton: FC<CustomButtonProps> = ({
         bg: "primary.hover",
       }}
       p={4}
-      onClick={action}
-      isLoading={isLoading}
       {...props}
     >
       {children}
--- src/components/CustomInput/index.tsx
+++ src/components/CustomInput/index.tsx
@@ -1,18 +1,20 @@
-import React from "react";
-import { Input, InputProps } from "@chakra-ui/react";
+import { forwardRef, useRef } from "react";
+import { Input, InputProps, useMergeRefs } from "@chakra-ui/react";
 
-interface CustomInputProps extends InputProps {}
+const CustomInput = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const mergeRef = useMergeRefs(inputRef, ref);
 
-const CustomInput: React.FC<CustomInputProps> = ({ ...props }) => {
   return (
     <Input
       py={2}
       border="1px solid #D0D5DD"
       boxShadow="0px 1px 2px 0px #1018280A"
       borderRadius="12px"
+      ref={mergeRef}
       {...props}
     />
   );
-};
+});
 
 export default CustomInput;
