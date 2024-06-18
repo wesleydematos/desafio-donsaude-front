b392fbfe6e4724ce785be0e3cfe8a45ddadcd0ad
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-06-18 11:52:12 -0300

    feat: create colaborators

--- src/pages/Colaborator/New/index.tsx
+++ src/pages/Colaborator/New/index.tsx
@@ -14,8 +14,14 @@ import { PiCaretLeftLight } from "react-icons/pi";
 import { CustomButton, CustomInput, FormBackground } from "../../../components";
 import { RiEyeCloseLine } from "react-icons/ri";
 import { AiOutlineEye } from "react-icons/ai";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { createColaboratorSchema } from "../../../schemas/colaboratorSchema";
+import { useColaborator } from "../../../contexts/ColaboratorsContext";
+import { ICreateColaborator } from "../../../contexts/ColaboratorsContext/interface";
 
 export default function NewColaborator() {
+  const { createColaborator } = useColaborator();
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
@@ -23,6 +29,24 @@ export default function NewColaborator() {
     type === "password"
       ? setShowPassword(!showPassword)
       : setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    resolver: yupResolver(createColaboratorSchema),
+  });
+
+  const clearInputs = () => {
+    reset();
+  };
+
+  const postColaborator: SubmitHandler<ICreateColaborator> = async (values) => {
+    await createColaborator({ ...values });
+    clearInputs();
   };
 
   return (
@@ -47,20 +71,32 @@ export default function NewColaborator() {
         <Text lineHeight="30px">Novo colaborador</Text>
       </Flex>
 
-      <FormBackground w="full">
+      <FormBackground w="full" onSubmit={handleSubmit(postColaborator)}>
         <Flex w="full" gap={5}>
           <FormControl>
             <FormLabel fontWeight={700} color="grey.bold">
               Nome
             </FormLabel>
-            <CustomInput placeholder="Digite o nome completo" />
+            <CustomInput
+              placeholder="Digite o nome completo"
+              {...register("name")}
+            />
+            {errors.name && (
+              <Text color="primary.base">{errors.name.message}</Text>
+            )}
           </FormControl>
 
           <FormControl>
             <FormLabel fontWeight={700} color="grey.bold">
               CPF
             </FormLabel>
-            <CustomInput placeholder="Apenas números" />
+            <CustomInput
+              placeholder="Apenas números"
+              {...register("documentNumber")}
+            />
+            {errors.documentNumber && (
+              <Text color="primary.base">{errors.documentNumber.message}</Text>
+            )}
           </FormControl>
         </Flex>
 
@@ -69,14 +105,20 @@ export default function NewColaborator() {
             <FormLabel fontWeight={700} color="grey.bold">
               Telefone
             </FormLabel>
-            <CustomInput placeholder="DDD + número" />
+            <CustomInput placeholder="DDD + número" {...register("phone")} />
+            {errors.phone && (
+              <Text color="primary.base">{errors.phone.message}</Text>
+            )}
           </FormControl>
 
           <FormControl>
             <FormLabel fontWeight={700} color="grey.bold">
               E-mail
             </FormLabel>
-            <CustomInput placeholder="Digite aqui" />
+            <CustomInput placeholder="Digite aqui" {...register("email")} />
+            {errors.email && (
+              <Text color="primary.base">{errors.email.message}</Text>
+            )}
           </FormControl>
         </Flex>
 
@@ -89,6 +131,7 @@ export default function NewColaborator() {
               <CustomInput
                 placeholder="Digite aqui"
                 type={showPassword ? "text" : "password"}
+                {...register("password")}
               />
               <InputRightElement width="4.5rem">
                 <IconButton
@@ -102,6 +145,9 @@ export default function NewColaborator() {
                 />
               </InputRightElement>
             </InputGroup>
+            {errors.password && (
+              <Text color="primary.base">{errors.password.message}</Text>
+            )}
           </FormControl>
 
           <FormControl>
@@ -112,6 +158,7 @@ export default function NewColaborator() {
               <CustomInput
                 placeholder="Digite novamente aqui"
                 type={showConfirmPassword ? "text" : "password"}
+                {...register("confirmPassword")}
               />
               <InputRightElement width="4.5rem">
                 <IconButton
@@ -131,6 +178,9 @@ export default function NewColaborator() {
                 />
               </InputRightElement>
             </InputGroup>
+            {errors.confirmPassword && (
+              <Text color="primary.base">{errors.confirmPassword.message}</Text>
+            )}
           </FormControl>
         </Flex>
         <FormControl>
@@ -151,12 +201,13 @@ export default function NewColaborator() {
             color="black"
             bg="white"
             _hover={{ color: "grey.bold" }}
+            onClick={clearInputs}
           >
             Cancelar
           </Button>
           <CustomButton
             type="submit"
-            // isLoading={isSubmitting}
+            isLoading={isSubmitting}
             w={{ base: "fit-content", xl: "247px" }}
             borderRadius="500px"
           >
